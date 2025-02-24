c5c9002c72b51d0cc6ed19ab03043cc30539a56c
Mateo-Ramosb <mateo.ramosbaena@unitecnar.edu.co>
2025-02-24 11:40:44 -0500

    Add files via upload

---
+++ Actividad 2 - Herencia - python/Main.py
@@ -0,0 +1,6 @@
+from automovil import Automovil
+
+# Creación de un objeto Automovil
+Automovil_1 = Automovil("15-03-2022", "ZHWUC1ZD2NLA12345", "LAM12345678X2022", 
+                         "Lamborghini", "Aventador LP 780-4 Ultimae", 550000)
+Automovil_1.mostrar_los_datos()
